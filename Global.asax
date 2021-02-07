a8fd2ddc181a5b8a0be6f0fc3b156504859ecd25
NaurisBr <68430762+NaurisBr@users.noreply.github.com>
2021-02-07 18:48:26 +0000

    Initial commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eatsy_app.MvcApplication" Language="C#" %>
---
+++ ViewModel/CustomerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eatsy_app.ViewModel
+{
+    public class CustomerViewModel
+    {
+    }
+}
