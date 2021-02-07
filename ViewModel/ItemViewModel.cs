1277edbc03f39f1e33f9d261d3d1d48d704c95a6
NaurisBr <68430762+NaurisBr@users.noreply.github.com>
2021-02-07 18:51:33 +0000

    fetching customer data

--- ViewModel/ItemViewModel.cs
+++ ViewModel/ItemViewModel.cs
@@ -7,5 +7,11 @@ namespace eatsy_app.ViewModel
 {
     public class ItemViewModel
     {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public decimal ItemPrice { get; set; }
+
+
+
     }
 }
--- ViewModel/PaymentTypeViewModel.cs
+++ ViewModel/PaymentTypeViewModel.cs
@@ -7,5 +7,9 @@ namespace eatsy_app.ViewModel
 {
     public class PaymentTypeViewModel
     {
+        public int PaymentTypeId { get; set; }
+        public string PaymentTypeName { get; set; }
+
+
     }
 }
